d88b2d79bdc49e63627da35cd4ed96a9f65b2cc3
marciomarquessouza <mms.processos@gmail.com>
2026-03-27 19:03:39 +0100

    Add Storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,47 @@
+import type { Preview } from "@storybook/nextjs-vite";
+import "../src/app/globals.css";
+import {
+  geistSans,
+  geistMono,
+  josefinSans,
+  russoOne,
+  specialElite,
+  unifrakturMaguntia,
+} from "../src/app/fonts";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: "todo",
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div
+        className={`
+          ${geistMono.variable}
+          ${geistSans.variable}
+          ${josefinSans.variable}
+          ${russoOne.variable}
+          ${specialElite.variable}
+          ${unifrakturMaguntia.variable}
+          antialiased
+        `}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default preview;
---
+++ src/gameUI/components/Dialogues/Dialogue.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Dialogue } from ".";
+import { useState } from "react";
+import { events } from "@/events/events";
+import { ACTORS } from "@/constants/game";
+import { Button } from "@/components/Button";
+
+const meta: Meta<typeof Dialogue> = {
+  title: "Game/UI/Dialogue",
+  component: Dialogue,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Dialogue>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const ClickExample: StoryObj = {
+  render: () => {
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+      events.game.async.emitAsync("dialogue/show", {
+        lines: [{ text: "Test", type: "dialogue", character: ACTORS.PLAYER }],
+      });
+      setCount((current) => current + 1);
+    };
+
+    return (
+      <div className="p-4">
+        <Button label="Open" onClick={handleClick} />
+        <p className="mt-2">Clicks: {count}</p>
+        <Dialogue />
+      </div>
+    );
+  },
+};
