bbefe6f435aa1475339bf6dd5ad3ced35465e847
marciomarquessouza <mms.processos@gmail.com>
2026-03-28 17:38:12 +0100

    feat: add Dialogue components including Alternatives, DialogueCTA, DialogueLines, and InputText

--- src/gameUI/components/Dialogues/components/DialogueCTA.tsx
+++ src/gameUI/components/Dialogues/components/DialogueCTA.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { Button } from "../../../components/Button";
+import { Button } from "../../../../components/Button";
 import { InteractionTypes } from "@/libs/dialogues/types";
 
 export interface DialogueCTAProps {
