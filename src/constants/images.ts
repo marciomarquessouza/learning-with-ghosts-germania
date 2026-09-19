77391f28a9cdf84dc1c5141f088ea8d7b5415f79
marciomarquessouza <mms.processos@gmail.com>
2026-09-19 11:29:38 +0200

    Add new logo SVG files and update image constants
    - Added logo-header.svg and logo-inverted.svg to the home page UI.
    - Updated image constants in images.ts to reference the new SVG logos.

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,6 +1,7 @@
 // pages images
 // HOME PAGE IMAGES
-export const MAIN_LOGO = "/ui/home_page/main-logo.png";
+export const MAIN_LOGO = "/ui/home_page/logo-header.svg";
+export const MAIN_LOGO_INVERTED = "/ui/home_page/logo-inverted.svg";
 export const HERO_IMAGE = "/ui/home_page/marlene-hero.png";
 // LOADING PAGE IMAGES
 export const GHOST_LOADER_BODY = "/ui/loading_screen/ghost-loader-body.png";
